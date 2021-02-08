56cb1b9b926fc6a8b021e72d84aa3cd43ad0707e
tsellers1990 <tsellers1990@gmail.com>
2021-02-08 13:51:33 -0700

    initial push to gh

---
+++ logic.js
@@ -0,0 +1,25 @@
+//input # of new students wanted, counter = # of students
+import inquirer from "inquirer"
+//for loop {
+//if counter > 0 {
+//connect to slack channel (fsf-requests)
+//listen for new posts
+//when new post seen, click nested link and message that user "me"
+//counter-- }
+//else return}
+
+function main() {
+
+
+  counter = number;
+
+  while (number > 0) {
+    //listen for new posts
+    //when new post seen, click nested link and message that admin "me"
+  }
+
+
+
+}
+
+main()
