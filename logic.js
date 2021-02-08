80f8b0ed860a18d5ab9871cda907af8ffab1c3d7
tsellers1990 <tsellers1990@gmail.com>
2021-02-08 14:00:33 -0700

    added a validate to make sure the user typed in a #

--- logic.js
+++ logic.js
@@ -1,5 +1,5 @@
 //input # of new students wanted, counter = # of students
-import inquirer from "inquirer"
+const inquirer = require("inquirer")
 //for loop {
 //if counter > 0 {
 //connect to slack channel (fsf-requests)
@@ -10,13 +10,27 @@ import inquirer from "inquirer"
 
 function main() {
 
+    inquirer.prompt({
+        type: "input",
+        name: "number",
+        message: "How many students do you need?",
+        validate: function (input) {
+            //if user inputs a number return true
+            if (isNaN(input)){
+                return false
+            } else return true
+            //else return false
+        }
+    }).then( data => {
+        counter = data.number;
 
-  counter = number;
+        while (number > 0) {
+          //listen for new posts
+          //when new post seen, click nested link and message that admin "me"
+        }
+    })
 
-  while (number > 0) {
-    //listen for new posts
-    //when new post seen, click nested link and message that admin "me"
-  }
+
 
 
 
